9e26dd6779b2015c0985b77975040bd016e4c4f6
denios.vasilios@gmail.com <denios.vasilios@gmail.com>
2025-05-20 22:24:21 +0300

    feat: UTs for Authorization domain and Core

---
+++ AuthorizationService.DomainUT/Model/AuthorizationUserTest.cs
@@ -0,0 +1,27 @@
+﻿using AuthorizationService.Domain.Model;
+
+namespace AuthorizationService.DomainUT.Model;
+
+public class AuthorizationUserTest
+{
+    [TestCase]
+    public void Constructor()
+    {
+        var id = Guid.NewGuid();
+        var login = "login";
+        var password = new byte[] { 1, 2, 3 };
+        var createDateTime = new DateTime(2025, 5, 20, 22, 19, 0);
+        var editDateTime = new DateTime(2025, 5, 20, 22, 20, 0);
+
+        var user = new AuthorizationUser(id, login, password, createDateTime, editDateTime);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(user.Id, Is.EqualTo(id));
+            Assert.That(user.Login, Is.EqualTo(login));
+            Assert.That(user.Password, Is.EqualTo(password));
+            Assert.That(user.CreateDateTime, Is.EqualTo(createDateTime));
+            Assert.That(user.EditDateTime, Is.EqualTo(editDateTime));
+        });
+    }
+}
---
+++ CoreUT/Dto/ResultTest.cs
@@ -0,0 +1,20 @@
+﻿using Core.Dto;
+
+namespace CoreUT.Dto;
+
+public class ResultTest
+{
+    [TestCase]
+    public void IsSuccessful_NoError()
+    {
+        var result = new Result();
+        Assert.That(result.IsSuccessful, Is.True);
+    }
+
+    [TestCase]
+    public void IsSuccessful_Error()
+    {
+        var result = new Result() { Error = "Error" };
+        Assert.That(result.IsSuccessful, Is.False);
+    }
+}
